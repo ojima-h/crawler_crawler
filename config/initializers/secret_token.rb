73c7ac036ae6e394d61c2cf80883d04f76c32423
Ojima Hikaru <ojiam.h@gmail.com>
2013-03-12 20:02:02 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrawlerCrawler::Application.config.secret_token = '8ae340c93fd88a0621c9704f8f3f617eebc43e0c9c205d3adc8d8e83b1a171949b9ce7c1836c1e73fecb8a7bffcf6f02051873618e6c94b7738de415ff1bd3ec'
